b97bed70064334e67ff3e7718029e2ea55e0e752
Margaret <margaret.morgane@gmail.com>
2015-04-04 18:35:10 +0000

    Web app created with Railsbridge Boston

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suggestotron::Application.config.secret_key_base = '92e23d89872244a89e078eb0a74bf26f22475ec2e50ba97cf9b22a45a6b8e916067d85d87b91fd3a17ef764b6bf8e46d8e6e4c934483b6e968c3a0f518202dfe'
